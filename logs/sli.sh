0c8580dae2c86851d194bb3363c6fc702beb2254
harol5 <hrcode95@gmail.com>
2022-12-18 17:01:05 -0500

    sli script

---
+++ logs/sli.sh
@@ -0,0 +1,37 @@
+httpCodes=$(grep Response rollingLogFile.2022-12-14.log | cut -f 1 -d ,| cut -f 2 -d : | cut -f 2 -d [ | cut -f 1 -d ] | cut -f 1 -d ' ')
+executionTime=$(grep execution rollingLogFile.2022-12-15.log | cut -f 2 -d , | cut -f 4 -d ' ')
+
+httpRequestTotal=0
+httpFailures=0
+executionFailures=0
+
+# then loop through the log data and perform any necessary operations to initialize your variables
+
+for code in $httpCodes
+do
+        ((httpRequestTotal++))
+        if [ $code -eq 500 ]
+        then
+                ((httpFailures++))
+        fi
+done
+
+for time in $executionTime
+do
+        if (( $(echo "$time > 200" |bc -l) ));
+        then
+                ((executionFailures++))
+        fi
+done
+
+# save the SLI value and return it
+
+httpSuccess=$(($httpRequestTotal - $httpFailures))
+resultHttpSuccess=$(echo "scale=2; $httpSuccess / $httpRequestTotal" | bc) # this option might not be available via gitbash on your local computer.
+
+executionSuccess=$(($httpRequestTotal - $executionFailures))
+resultExecutionSuccess=$(echo "scale=2; $executionSuccess / $httpRequestTotal" | bc)
+
+echo "total requests: $httpRequestTotal"
+echo "HTTP success rate: $resultHttpSuccess%"
+echo "Execution success rate: $resultExecutionSuccess%"
